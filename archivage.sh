b9e37fa514a2caef6d7b6bcd211bd6d60d550121
pllensa <pllensa@myges.fr>
2025-06-13 16:36:15 +0200

    feat: archivage ligne par ligne

--- archivage.sh
+++ archivage.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
 # a faire par logrotate et cron
-find $(pwd) -name change-droit.log -mtime +7 -exec cat change-droit.log >> change-droit.archive.log && cat "" > change-droit.log
+# sinon gros deg en reposant sur la conversion en epoch des dates
+for i in $(cat $(pwd)/change-droit.log); do
+	if [ $($(( ($(date --iso +%s) - $(date --date=$(awk '{print$1}') +%s )) /(60*60*24)))) > 7  ] then
+    head -n 1 > change-droit.archive.log
+    sed -i '0d' change-droit.log
+  fi
 
+for i in $(cat $(pwd)/change-droit.archive.log); do
+	if [ $($(( ($(date --iso +%s) - $(date --date=$(awk '{print$1}') +%s )) /(60*60*24)))) > 90  ] then
+    sed -i '0d' change-droit.log
+  fi
